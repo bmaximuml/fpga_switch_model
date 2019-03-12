80f4d26622db46ef0d915c2a8bf87eb7fde6a188
Benji Levine <benjilev08@gmail.com>
2019-03-12 13:49:05 +0000

    Use setup.py to install mininet

--- setup.py
+++ setup.py
@@ -1,4 +1,27 @@
+import os
+import subprocess
+
+
 from setuptools import setup
+from setuptools.command.install import install
+
+
+class PreInstallCommand(install):
+    """Provides a wrapper to install mininet when the package is installed"""
+
+    def run(self):
+        # Run this first so the install stops if it fails
+        self._install_mininet()
+        # Run the standard install
+        install.run(self)
+
+    def _install_mininet(self):
+        subprocess.call('mininet/util/install.sh -nfv', shell=True)
+        if 'mininet/mininet' not in os.environ['PATH']:
+            os.environ['PATH'] = os.environ['PATH'] + ':' + \
+                                 os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                              'mininet', 'mininet')
+
 
 setup(
     name='fpga_switch_model',
@@ -13,4 +36,5 @@ setup(
         [console_scripts]
         fpga_switch_model=fpga_switch_model:cli
     ''',
+    cmdclass={'install': PreInstallCommand},
 )
