84f19aae5c59fd0992260131dd2f024a0c04f941
Benji Levine <benjilev08@gmail.com>
2019-05-06 10:45:33 +0100

    Separate performance unit tests

--- test/unit/test_performance_tests_unit.py
+++ test/unit/test_performance_tests_unit.py
@@ -8,14 +8,15 @@ from mininet.net import Mininet
 from mininet.link import TCLink
 from mininet.node import CPULimitedHost
 
+
 class TestTestCloudFpga(unittest.TestCase):
     """Test the test_cloud_fpga function"""
     def test_simple(self):
-        topo_0 = TreeTopoGeneric(spread=2, depth=7, bandwidth=10, delay='1ms', loss=0, fpga=None)
-        topo_1 = TreeTopoGeneric(spread=2, depth=7, bandwidth=10, delay='1ms', loss=0, fpga=1)
-        topo_2 = TreeTopoGeneric(spread=2, depth=7, bandwidth=10, delay='1ms', loss=0, fpga=2)
-        topo_3 = TreeTopoGeneric(spread=2, depth=7, bandwidth=10, delay='1ms', loss=0, fpga=3)
-        topo_4 = TreeTopoGeneric(spread=2, depth=7, bandwidth=10, delay='1ms', loss=0, fpga=4)
+        topo_0 = TreeTopoGeneric(spread=3, depth=4, bandwidth=10, delay='1ms', loss=0, fpga=None)
+        topo_1 = TreeTopoGeneric(spread=2, depth=7, bandwidth=10, delay='1ms', loss=1, fpga=None)
+        topo_2 = TreeTopoGeneric(spread=2, depth=6, bandwidth=1, delay='1ms', loss=0, fpga=None)
+        topo_3 = TreeTopoGeneric(spread=5, depth=2, bandwidth=10, delay='10ms', loss=1, fpga=None)
+        topo_4 = TreeTopoGeneric(spread=3, depth=5, bandwidth=10, delay='1ms', loss=0, fpga=None)
 
         topologies = [topo_0, topo_1, topo_2, topo_3, topo_4]
 
