9b8b6cb955926bf43a6a58d568ac79d85c75454b
Benji Levine <benjilev08@gmail.com>
2019-05-06 01:25:21 +0100

    Add additional halve_delay tests

--- test/test_mininet_functions.py
+++ test/test_mininet_functions.py
@@ -9,6 +9,9 @@ class TestHalveDelay(unittest.TestCase):
     """Test the halve_delay function"""
     def test_simple(self):
         self.assertEqual('5.0ms', halve_delay('10ms'))
+        self.assertEqual('25.0ms', halve_delay('50ms'))
+        self.assertEqual('2.5ms', halve_delay('5ms'))
+        self.assertEqual('50.0ms', halve_delay('100ms'))
 
 
 if __name__ == '__main__':
