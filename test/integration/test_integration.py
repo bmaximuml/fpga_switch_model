7496a6d312ba12f964977305f91940ae3c6b2683
Benji Levine <benjilev08@gmail.com>
2019-05-06 11:43:12 +0100

    More tests

--- test/integration/test_integration.py
+++ test/integration/test_integration.py
@@ -13,32 +13,33 @@ from mininet.node import CPULimitedHost
 class TestTreeTopoGenericPoisson(unittest.TestCase):
     """Test the TreeTopoGeneric class with the get_poisson_delay function"""
     def test_simple(self):
-        topo_0 = TreeTopoGeneric(spread=2, depth=7, bandwidth=10, delay='1ms', loss=0, fpga=None,
+        topo_0 = TreeTopoGeneric(spread=2, depth=4, bandwidth=10, delay='1ms', loss=0, fpga=None,
                                  poisson=True)
-        topo_1 = TreeTopoGeneric(spread=3, depth=5, bandwidth=10, delay='1ms', loss=1, fpga=None,
-                                 poisson=True)
-        topo_2 = TreeTopoGeneric(spread=2, depth=7, bandwidth=10, delay='4ms', loss=2, fpga=None,
-                                 poisson=True)
-        topo_3 = TreeTopoGeneric(spread=3, depth=2, bandwidth=1, delay='1ms', loss=50, fpga=None,
-                                 poisson=True)
-        topo_4 = TreeTopoGeneric(spread=4, depth=4, bandwidth=100, delay='1ms', loss=0, fpga=None,
-                                 poisson=True)
-        topo_5 = TreeTopoGeneric(spread=3, depth=5, bandwidth=10, delay='4ms', loss=2, fpga=None,
-                                 poisson=True)
-        topo_6 = TreeTopoGeneric(spread=1, depth=4, bandwidth=10, delay='1ms', loss=50, fpga=None,
-                                 poisson=True)
-        topo_7 = TreeTopoGeneric(spread=2, depth=7, bandwidth=10, delay='1ms', loss=0, fpga=None,
-                                 poisson=True)
-        topo_8 = TreeTopoGeneric(spread=4, depth=6, bandwidth=10, delay='1ms', loss=0, fpga=1,
-                                 poisson=True)
-        topo_9 = TreeTopoGeneric(spread=3, depth=4, bandwidth=10, delay='1ms', loss=0, fpga=2,
-                                 poisson=True)
-        topo_10 = TreeTopoGeneric(spread=5, depth=2, bandwidth=10, delay='1ms', loss=0, fpga=3,
-                                 poisson=True)
-        topo_11 = TreeTopoGeneric(spread=2, depth=7, bandwidth=10, delay='1ms', loss=0, fpga=4,
-                                 poisson=True)
+        # topo_1 = TreeTopoGeneric(spread=3, depth=5, bandwidth=10, delay='1ms', loss=1, fpga=None,
+        #                          poisson=True)
+        # topo_2 = TreeTopoGeneric(spread=2, depth=7, bandwidth=10, delay='4ms', loss=2, fpga=None,
+        #                          poisson=True)
+        # topo_3 = TreeTopoGeneric(spread=3, depth=2, bandwidth=1, delay='1ms', loss=50, fpga=None,
+        #                          poisson=True)
+        # topo_4 = TreeTopoGeneric(spread=4, depth=4, bandwidth=100, delay='1ms', loss=0, fpga=None,
+        #                          poisson=True)
+        # topo_5 = TreeTopoGeneric(spread=3, depth=5, bandwidth=10, delay='4ms', loss=2, fpga=None,
+        #                          poisson=True)
+        # topo_6 = TreeTopoGeneric(spread=1, depth=4, bandwidth=10, delay='1ms', loss=50, fpga=None,
+        #                          poisson=True)
+        # topo_7 = TreeTopoGeneric(spread=2, depth=7, bandwidth=10, delay='1ms', loss=0, fpga=None,
+        #                          poisson=True)
+        # topo_8 = TreeTopoGeneric(spread=4, depth=6, bandwidth=10, delay='1ms', loss=0, fpga=1,
+        #                          poisson=True)
+        # topo_9 = TreeTopoGeneric(spread=3, depth=4, bandwidth=10, delay='1ms', loss=0, fpga=2,
+        #                          poisson=True)
+        # topo_10 = TreeTopoGeneric(spread=5, depth=2, bandwidth=10, delay='1ms', loss=0, fpga=3,
+        #                          poisson=True)
+        # topo_11 = TreeTopoGeneric(spread=2, depth=7, bandwidth=10, delay='1ms', loss=0, fpga=4,
+        #                          poisson=True)
 
-        topologies = [topo_0, topo_1, topo_2, topo_3, topo_4, topo_5, topo_6, topo_7, topo_8, topo_9, topo_10, topo_11]
+        topologies = [topo_0]
+        # topologies = [topo_0, topo_1, topo_2, topo_3, topo_4, topo_5, topo_6, topo_7, topo_8, topo_9, topo_10, topo_11]
 
         for topo in topologies:
             try:
@@ -54,16 +55,17 @@ class TestTreeTopoGenericPoisson(unittest.TestCase):
 class TestTreeTopoGenericHalveDelay(unittest.TestCase):
     """Test the TreeTopoGeneric class with the halve_delay function"""
     def test_simple(self):
-        topo_0 = TreeTopoGeneric(spread=2, depth=7, bandwidth=10, delay='3ms', loss=0, fpga=1)
-        topo_1 = TreeTopoGeneric(spread=3, depth=5, bandwidth=10, delay='10ms', loss=1, fpga=2)
-        topo_2 = TreeTopoGeneric(spread=2, depth=7, bandwidth=10, delay='4ms', loss=2, fpga=3)
-        topo_3 = TreeTopoGeneric(spread=3, depth=3, bandwidth=1, delay='2ms', loss=50, fpga=3)
-        topo_4 = TreeTopoGeneric(spread=4, depth=4, bandwidth=100, delay='1ms', loss=0, fpga=1)
-        topo_5 = TreeTopoGeneric(spread=3, depth=5, bandwidth=10, delay='4ms', loss=2, fpga=2)
-        topo_6 = TreeTopoGeneric(spread=1, depth=4, bandwidth=10, delay='20ms', loss=50, fpga=2)
-        topo_7 = TreeTopoGeneric(spread=2, depth=7, bandwidth=10, delay='1ms', loss=0, fpga=5)
+        topo_0 = TreeTopoGeneric(spread=2, depth=4, bandwidth=10, delay='3ms', loss=0, fpga=1)
+        # topo_1 = TreeTopoGeneric(spread=3, depth=5, bandwidth=10, delay='10ms', loss=1, fpga=2)
+        # topo_2 = TreeTopoGeneric(spread=2, depth=7, bandwidth=10, delay='4ms', loss=2, fpga=3)
+        # topo_3 = TreeTopoGeneric(spread=3, depth=3, bandwidth=1, delay='2ms', loss=50, fpga=3)
+        # topo_4 = TreeTopoGeneric(spread=4, depth=4, bandwidth=100, delay='1ms', loss=0, fpga=1)
+        # topo_5 = TreeTopoGeneric(spread=3, depth=5, bandwidth=10, delay='4ms', loss=2, fpga=2)
+        # topo_6 = TreeTopoGeneric(spread=1, depth=4, bandwidth=10, delay='20ms', loss=50, fpga=2)
+        # topo_7 = TreeTopoGeneric(spread=2, depth=7, bandwidth=10, delay='1ms', loss=0, fpga=5)
 
-        topologies = [topo_0, topo_1, topo_2, topo_3, topo_4, topo_5, topo_6, topo_7]
+        topologies = [topo_0]
+        # topologies = [topo_0, topo_1, topo_2, topo_3, topo_4, topo_5, topo_6, topo_7]
 
         for topo in topologies:
             try:
@@ -80,32 +82,33 @@ class TestTreeTopoGenericHalveDelay(unittest.TestCase):
 class TestTreeTopoGenericHalveDelayPoisson(unittest.TestCase):
     """Test the TreeTopoGeneric class with the get_poisson_delay and halve_delay functions"""
     def test_simple(self):
-        topo_0 = TreeTopoGeneric(spread=2, depth=7, bandwidth=10, delay='7ms', loss=0, fpga=4,
+        topo_0 = TreeTopoGeneric(spread=2, depth=6, bandwidth=10, delay='7ms', loss=0, fpga=4,
                                  poisson=True)
-        topo_1 = TreeTopoGeneric(spread=3, depth=5, bandwidth=10, delay='32ms', loss=1, fpga=2,
-                                 poisson=True)
-        topo_2 = TreeTopoGeneric(spread=2, depth=7, bandwidth=10, delay='4ms', loss=2, fpga=6,
-                                 poisson=True)
-        topo_3 = TreeTopoGeneric(spread=3, depth=3, bandwidth=1, delay='19ms', loss=50, fpga=2,
-                                 poisson=True)
-        topo_4 = TreeTopoGeneric(spread=4, depth=4, bandwidth=100, delay='14ms', loss=0, fpga=2,
-                                 poisson=True)
-        topo_5 = TreeTopoGeneric(spread=3, depth=5, bandwidth=10, delay='4ms', loss=2, fpga=4,
-                                 poisson=True)
-        topo_6 = TreeTopoGeneric(spread=1, depth=4, bandwidth=10, delay='12ms', loss=50, fpga=2,
-                                 poisson=True)
-        topo_7 = TreeTopoGeneric(spread=2, depth=7, bandwidth=10, delay='31ms', loss=0, fpga=5,
-                                 poisson=True)
-        topo_8 = TreeTopoGeneric(spread=4, depth=6, bandwidth=10, delay='1ms', loss=0, fpga=1,
-                                 poisson=True)
-        topo_9 = TreeTopoGeneric(spread=3, depth=4, bandwidth=10, delay='1ms', loss=0, fpga=2,
-                                 poisson=True)
-        topo_10 = TreeTopoGeneric(spread=5, depth=2, bandwidth=10, delay='11ms', loss=0, fpga=3,
-                                 poisson=True)
-        topo_11 = TreeTopoGeneric(spread=2, depth=7, bandwidth=10, delay='7ms', loss=0, fpga=4,
-                                 poisson=True)
+        # topo_1 = TreeTopoGeneric(spread=3, depth=5, bandwidth=10, delay='32ms', loss=1, fpga=2,
+        #                          poisson=True)
+        # topo_2 = TreeTopoGeneric(spread=2, depth=7, bandwidth=10, delay='4ms', loss=2, fpga=6,
+        #                          poisson=True)
+        # topo_3 = TreeTopoGeneric(spread=3, depth=3, bandwidth=1, delay='19ms', loss=50, fpga=2,
+        #                          poisson=True)
+        # topo_4 = TreeTopoGeneric(spread=4, depth=4, bandwidth=100, delay='14ms', loss=0, fpga=2,
+        #                          poisson=True)
+        # topo_5 = TreeTopoGeneric(spread=3, depth=5, bandwidth=10, delay='4ms', loss=2, fpga=4,
+        #                          poisson=True)
+        # topo_6 = TreeTopoGeneric(spread=1, depth=4, bandwidth=10, delay='12ms', loss=50, fpga=2,
+        #                          poisson=True)
+        # topo_7 = TreeTopoGeneric(spread=2, depth=7, bandwidth=10, delay='31ms', loss=0, fpga=5,
+        #                          poisson=True)
+        # topo_8 = TreeTopoGeneric(spread=4, depth=6, bandwidth=10, delay='1ms', loss=0, fpga=1,
+        #                          poisson=True)
+        # topo_9 = TreeTopoGeneric(spread=3, depth=4, bandwidth=10, delay='1ms', loss=0, fpga=2,
+        #                          poisson=True)
+        # topo_10 = TreeTopoGeneric(spread=5, depth=2, bandwidth=10, delay='11ms', loss=0, fpga=3,
+        #                          poisson=True)
+        # topo_11 = TreeTopoGeneric(spread=2, depth=7, bandwidth=10, delay='7ms', loss=0, fpga=4,
+        #                          poisson=True)
 
-        topologies = [topo_0, topo_1, topo_2, topo_3, topo_4, topo_5, topo_6, topo_7, topo_8, topo_9, topo_10, topo_11]
+        topologies = [topo_0]
+        # topologies = [topo_0, topo_1, topo_2, topo_3, topo_4, topo_5, topo_6, topo_7, topo_8, topo_9, topo_10, topo_11]
 
         for topo in topologies:
             try:
@@ -123,12 +126,13 @@ class TestTestCloudFpgaTestCloud(unittest.TestCase):
     """Test the test_cloud_fpga with the halve_delay function and the TreeTopoGeneric class"""
     def test_simple(self):
         topo_0 = TreeTopoGeneric(spread=3, depth=4, bandwidth=10, delay='1ms', loss=0, fpga=2)
-        topo_1 = TreeTopoGeneric(spread=2, depth=7, bandwidth=10, delay='1ms', loss=1, fpga=3)
-        topo_2 = TreeTopoGeneric(spread=2, depth=6, bandwidth=1, delay='1ms', loss=0, fpga=5)
-        topo_3 = TreeTopoGeneric(spread=5, depth=3, bandwidth=10, delay='10ms', loss=1, fpga=2)
-        topo_4 = TreeTopoGeneric(spread=3, depth=5, bandwidth=10, delay='1ms', loss=0, fpga=3)
+        # topo_1 = TreeTopoGeneric(spread=2, depth=7, bandwidth=10, delay='1ms', loss=1, fpga=3)
+        # topo_2 = TreeTopoGeneric(spread=2, depth=6, bandwidth=1, delay='1ms', loss=0, fpga=5)
+        # topo_3 = TreeTopoGeneric(spread=5, depth=3, bandwidth=10, delay='10ms', loss=1, fpga=2)
+        # topo_4 = TreeTopoGeneric(spread=3, depth=5, bandwidth=10, delay='1ms', loss=0, fpga=3)
 
-        topologies = [topo_0, topo_1, topo_2, topo_3, topo_4]
+        topologies = [topo_0]
+        # topologies = [topo_0, topo_1, topo_2, topo_3, topo_4]
 
         for i, topo in enumerate(topologies):
             Cleanup.cleanup()
@@ -149,16 +153,17 @@ class TestTestCloudFpgaHalveDelayPoisson(unittest.TestCase):
     def test_simple(self):
         topo_0 = TreeTopoGeneric(spread=3, depth=4, bandwidth=10, delay='1ms', loss=0, fpga=2
                                  , poisson=True)
-        topo_1 = TreeTopoGeneric(spread=2, depth=7, bandwidth=10, delay='1ms', loss=1, fpga=3
-                                 , poisson=True)
-        topo_2 = TreeTopoGeneric(spread=2, depth=6, bandwidth=1, delay='1ms', loss=0, fpga=5
-                                 , poisson=True)
-        topo_3 = TreeTopoGeneric(spread=5, depth=3, bandwidth=10, delay='10ms', loss=1, fpga=2
-                                 , poisson=True)
-        topo_4 = TreeTopoGeneric(spread=3, depth=5, bandwidth=10, delay='1ms', loss=0, fpga=3
-                                 , poisson=True)
+        # topo_1 = TreeTopoGeneric(spread=2, depth=7, bandwidth=10, delay='1ms', loss=1, fpga=3
+        #                          , poisson=True)
+        # topo_2 = TreeTopoGeneric(spread=2, depth=6, bandwidth=1, delay='1ms', loss=0, fpga=5
+        #                          , poisson=True)
+        # topo_3 = TreeTopoGeneric(spread=5, depth=3, bandwidth=10, delay='10ms', loss=1, fpga=2
+        #                          , poisson=True)
+        # topo_4 = TreeTopoGeneric(spread=3, depth=5, bandwidth=10, delay='1ms', loss=0, fpga=3
+        #                          , poisson=True)
 
-        topologies = [topo_0, topo_1, topo_2, topo_3, topo_4]
+        topologies = [topo_0]
+        # topologies = [topo_0, topo_1, topo_2, topo_3, topo_4]
 
         for i, topo in enumerate(topologies):
             Cleanup.cleanup()
--- test/unit/test_performance_tests_unit.py
+++ test/unit/test_performance_tests_unit.py
@@ -13,12 +13,13 @@ class TestTestCloudFpga(unittest.TestCase):
     """Test the test_cloud_fpga function"""
     def test_simple(self):
         topo_0 = TreeTopoGeneric(spread=3, depth=4, bandwidth=10, delay='1ms', loss=0, fpga=None)
-        topo_1 = TreeTopoGeneric(spread=2, depth=7, bandwidth=10, delay='1ms', loss=1, fpga=None)
-        topo_2 = TreeTopoGeneric(spread=2, depth=6, bandwidth=1, delay='1ms', loss=0, fpga=None)
-        topo_3 = TreeTopoGeneric(spread=5, depth=2, bandwidth=10, delay='10ms', loss=1, fpga=None)
-        topo_4 = TreeTopoGeneric(spread=3, depth=5, bandwidth=10, delay='1ms', loss=0, fpga=None)
+        # topo_1 = TreeTopoGeneric(spread=2, depth=7, bandwidth=10, delay='1ms', loss=1, fpga=None)
+        # topo_2 = TreeTopoGeneric(spread=2, depth=6, bandwidth=1, delay='1ms', loss=0, fpga=None)
+        # topo_3 = TreeTopoGeneric(spread=5, depth=2, bandwidth=10, delay='10ms', loss=1, fpga=None)
+        # topo_4 = TreeTopoGeneric(spread=3, depth=5, bandwidth=10, delay='1ms', loss=0, fpga=None)
 
-        topologies = [topo_0, topo_1, topo_2, topo_3, topo_4]
+        topologies = [topo_0]
+        # topologies = [topo_0, topo_1, topo_2, topo_3, topo_4]
 
         for i, topo in enumerate(topologies):
             Cleanup.cleanup()
