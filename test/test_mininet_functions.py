2f1c3f6bf1ec571f24be46302338309e56457312
Benji Levine <benjilev08@gmail.com>
2019-05-06 10:05:28 +0100

    Add TreeTopoGeneric test

--- test/test_mininet_functions.py
+++ test/test_mininet_functions.py
@@ -3,7 +3,12 @@
 import re
 import unittest
 
-from mininet_functions import halve_delay, get_poisson_delay
+from mininet.clean import Cleanup
+from mininet.net import Mininet
+from mininet.link import TCLink
+from mininet.node import CPULimitedHost
+
+from mininet_functions import halve_delay, get_poisson_delay, TreeTopoGeneric
 
 
 class TestHalveDelay(unittest.TestCase):
@@ -27,6 +32,36 @@ class TestPoissonDelay(unittest.TestCase):
             except ValueError:
                 self.assertEqual(True, False, 'Could not cast {} to float.'.format(match.group(1)))
             self.assertEqual(match.group(2), 'ms')
+
+
+class TestTreeTopoGeneric(unittest.TestCase):
+    """Test the TreeTopoGeneric class"""
+    def test_simple(self):
+        topo_0 = TreeTopoGeneric(spread=2, depth=7, bandwidth=10, delay='1ms', loss=0, fpga=None)
+        topo_1 = TreeTopoGeneric(spread=3, depth=5, bandwidth=10, delay='1ms', loss=1, fpga=None)
+        topo_2 = TreeTopoGeneric(spread=2, depth=7, bandwidth=10, delay='4ms', loss=2, fpga=None)
+        topo_3 = TreeTopoGeneric(spread=3, depth=2, bandwidth=1, delay='1ms', loss=50, fpga=None)
+        topo_4 = TreeTopoGeneric(spread=4, depth=4, bandwidth=100, delay='1ms', loss=0, fpga=None)
+        topo_5 = TreeTopoGeneric(spread=3, depth=5, bandwidth=10, delay='4ms', loss=2, fpga=None)
+        topo_6 = TreeTopoGeneric(spread=1, depth=4, bandwidth=10, delay='1ms', loss=50, fpga=None)
+        topo_7 = TreeTopoGeneric(spread=2, depth=7, bandwidth=10, delay='1ms', loss=0, fpga=None)
+        topo_8 = TreeTopoGeneric(spread=4, depth=6, bandwidth=10, delay='1ms', loss=0, fpga=1)
+        topo_9 = TreeTopoGeneric(spread=3, depth=4, bandwidth=10, delay='1ms', loss=0, fpga=2)
+        topo_10 = TreeTopoGeneric(spread=5, depth=2, bandwidth=10, delay='1ms', loss=0, fpga=3)
+        topo_11 = TreeTopoGeneric(spread=2, depth=7, bandwidth=10, delay='1ms', loss=0, fpga=4)
+
+        topologies = [topo_0, topo_1, topo_2, topo_3, topo_4, topo_5, topo_6, topo_7, topo_8, topo_9, topo_10, topo_11]
+
+        for topo in topologies:
+            try:
+                Cleanup.cleanup()
+                net = Mininet(topo=topo, host=CPULimitedHost, link=TCLink, autoStaticArp=True)
+                net.start()
+                self.assertEqual(True, True)
+            except Exception as ex:
+                self.assertEqual(True, False, 'TestTreeTopoGeneric rose an Exception of type {}.'.format(type(ex).__name__))
+            finally:
+                Cleanup.cleanup()
 
 
 if __name__ == '__main__':
