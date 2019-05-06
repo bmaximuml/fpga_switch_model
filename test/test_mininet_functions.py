3fd217dd807f42038f9c032c500969c9677b87b9
Benji Levine <benjilev08@gmail.com>
2019-05-06 01:16:42 +0100

    Conform to PEP8

--- test/test_mininet_functions.py
+++ test/test_mininet_functions.py
@@ -4,10 +4,12 @@ import unittest
 
 from mininet_functions import halve_delay
 
+
 class TestHalveDelay(unittest.TestCase):
     """Test the halve_delay function"""
-    def testSimple(self):
+    def test_simple(self):
         self.assertEqual('5.0ms', halve_delay('10ms'))
+
 
 if __name__ == '__main__':
     unittest.main()
