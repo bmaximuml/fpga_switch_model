f62060dd2152488843ca717019c9f7c629189500
Benji Levine <benjilev08@gmail.com>
2019-05-06 01:16:42 +0100

    Conform to PEP8

--- test/runner.py
+++ test/runner.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python
 
-import click
 import os
-
 from unittest import TextTestRunner, TestLoader
+import click
 
 
 @click.command()
 @click.option('-v', '--verbose', is_flag=True, help='verbose output')
 @click.option('-t', '--test-dir', default=os.path.dirname(os.path.realpath(__file__)), type=str,
               help='The directory to look for tests in')
-def runTests(test_dir, verbose):
+def run_tests(test_dir, verbose):
     """discover and run all tests in test_dir"""
-    testSuite = TestLoader().discover(test_dir)
+    test_suite = TestLoader().discover(test_dir)
     verbosity = 2 if verbose else 1
     # run tests
-    result = TextTestRunner(verbosity=verbosity).run(testSuite)
+    result = TextTestRunner(verbosity=verbosity).run(test_suite)
     if result.wasSuccessful():
         return 0
     return 1
 
+
 if __name__ == '__main__':
     # get the directory containing tests
-    runTests()
+    run_tests()
